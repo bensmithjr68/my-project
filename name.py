3ab0fc60e4d927684f7c6a97b1d5a9177a7db371
bensmithjr68 <bensmithjr@gmail.com>
2016-05-31 20:25:21 +0000

    Add files via upload

---
+++ name.py
@@ -0,0 +1,10 @@
+name = "ben smith"
+print(name.title())
+print(name.upper())
+print(name.lower())
+firstn = "ben"
+lastn = "smith"
+fullname = firstn + " " + lastn
+print("First Name ", firstn.title())
+print("Last Name ", lastn.title())
+print("Full Name ", fullname.title())

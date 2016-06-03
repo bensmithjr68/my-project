dca6874dc89eb086fb8178495f3485f0f6f1f647
Ben Smith <bensmithjr@gmail.com>
2016-06-03 07:13:34 -0500

    June 2

--- parrot.py
+++ parrot.py
@@ -1,6 +1,10 @@
 prompt = "\nTell me something, and I will repeat it back to you:"
 prompt += "\nEnter 'quit' to end program. "
-message =  " "
-while message != 'quit':
+active = True
+while active:
 	message = input(prompt)
-	print(message)
+	if message == 'quit':
+		active = False
+	else:
+		print(message)
+

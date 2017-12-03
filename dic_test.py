52f57734bd4fe5da7813b4068302ea8c9187f44d
bensmithjr68 <bensmithjr@gmail.com>
2017-12-02 18:59:06 -0600

    Delete dic_test.py

--- dic_test.py
+++
@@ -1,7 +0,0 @@
-friend_1 = {}
-friend_1['age'] = '32'
-friend_1['sex'] = 'female'
-friend_1['eye color'] = 'green'
-for trait, fdesc in friend_1.items():
-	print(trait.title() + ": " + fdesc.title())
-
